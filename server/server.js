30375e9bdec1aec2a14a1ec0ac30a25eb88360b4
sreeja97 <govardhanasrija@gmail.com>
2022-10-06 15:00:30 -0500

    login code

--- server/server.js
+++ server/server.js
@@ -3,6 +3,7 @@ const cors = require("cors");
 
 const app = express();
 
+
 // const db = require("./models");
 // db.sequelize.sync({ force: true }).then(() => {
 //   console.log("Drop and re-sync db."); // TODO need to check this
@@ -13,13 +14,21 @@ var corsOptions = {
   origin: "http://localhost:8081"
 };
 
+
+
+ 
+
 app.use(cors(corsOptions));
+
 
 // parse requests of content-type - application/json
 app.use(express.json());
 
 // parse requests of content-type - application/x-www-form-urlencoded
 app.use(express.urlencoded({ extended: true }));
+
+
+
 
 // simple route
 app.get("/", (req, res) => {
@@ -31,3 +40,27 @@ const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}.`);
 });
+
+/**
+ * Session Configuration (New!)
+ */
+
+ const { auth } = require('express-openid-connect');
+
+ const config = {
+   authRequired: false,
+   auth0Logout: true,
+   secret: 'b769364f54ca28b2c2b66af3fcbb22389a49dc6ca5151a815ce469faee6d110',
+   baseURL: 'http://localhost:8080',
+   clientID: '332BfKyFD0pV2xtTvI4kdk1kjkIDvLJc',
+   issuerBaseURL: 'https://dev-6c9z8pw1.us.auth0.com'
+ };
+ 
+ // auth router attaches /login, /logout, and /callback routes to the baseURL
+ app.use(auth(config));
+ 
+ // req.isAuthenticated is provided from the auth router
+ app.get('/', (req, res) => {
+   res.send(req.oidc.isAuthenticated() ? 'Logged in' : 'Logged out');
+ });
+ 
