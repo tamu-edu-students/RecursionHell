bba859c4fda518b4a5856878bf2599ba1a603ae4
avdhi7 <avdhi.shah@tamu.edu>
2022-10-18 22:38:35 -0500

    user screens

---
+++ frontend/src/app/layout/screen1/teacher-reports.ts
@@ -0,0 +1,102 @@
+export const teacherReports = [
+    {
+        id: 'Report 1',
+        description: 'This is report 1',
+        date: 'Jan 1, 2023',
+    },
+    {
+        id: 'Report 2',
+        description: 'This is report 2',
+        date: 'Jan 1, 2023',
+    },
+    {
+        id: 'Report 3',
+        description: 'This is report 3',
+        date: 'Jan 1, 2023',
+    },
+    {
+        id: 'Report 4',
+        description: 'This is report 4',
+        date: 'Jan 1, 2023',
+    },
+    {
+        id: 'Report 5',
+        description: 'This is report 5',
+        date: 'Jan 1, 2023',
+    },
+    {
+        id: 'Report 1',
+        description: 'This is report 1',
+        date: 'Jan 1, 2023',
+    },
+    {
+        id: 'Report 2',
+        description: 'This is report 2',
+        date: 'Jan 1, 2023',
+    },
+    {
+        id: 'Report 3',
+        description: 'This is report 3',
+        date: 'Jan 1, 2023',
+    },
+    {
+        id: 'Report 4',
+        description: 'This is report 4',
+        date: 'Jan 1, 2023',
+    },
+    {
+        id: 'Report 5',
+        description: 'This is report 5',
+        date: 'Jan 1, 2023',
+    },
+    {
+        id: 'Report 1',
+        description: 'This is report 1',
+        date: 'Jan 1, 2023',
+    },
+    {
+        id: 'Report 2',
+        description: 'This is report 2',
+        date: 'Jan 1, 2023',
+    },
+    {
+        id: 'Report 3',
+        description: 'This is report 3',
+        date: 'Jan 1, 2023',
+    },
+    {
+        id: 'Report 4',
+        description: 'This is report 4',
+        date: 'Jan 1, 2023',
+    },
+    {
+        id: 'Report 5',
+        description: 'This is report 5',
+        date: 'Jan 1, 2023',
+    },
+    {
+        id: 'Report 1',
+        description: 'This is report 1',
+        date: 'Jan 1, 2023',
+    },
+    {
+        id: 'Report 2',
+        description: 'This is report 2',
+        date: 'Jan 1, 2023',
+    },
+    {
+        id: 'Report 3',
+        description: 'This is report 3',
+        date: 'Jan 1, 2023',
+    },
+    {
+        id: 'Report 4',
+        description: 'This is report 4',
+        date: 'Jan 1, 2023',
+    },
+    {
+        id: 'Report 5',
+        description: 'This is report 5',
+        date: 'Jan 1, 2023',
+    },
+];
---
+++ frontend/src/app/layout/screen2/teachers.ts
@@ -0,0 +1,23 @@
+export const teachers = [
+    {
+        id: 'Teacher 1',
+    },
+    {
+        id: 'Teacher 2',
+    },
+    {
+        id: 'Teacher 3',
+    },
+    {
+        id: 'Teacher 4',
+    },
+    {
+        id: 'Teacher 5',
+    },
+    {
+        id: 'Teacher 6',
+    },
+    {
+        id: 'Teacher 7',
+    },
+];
